046caae6779ab9d70386aba53203312d2d03658a
Ilia Dev <bridatal@gmail.com>
2023-11-27 17:15:58 +0400

    First edition

---
+++ js/script.js
@@ -0,0 +1,40 @@
+"use strict";
+window.addEventListener("DOMContentLoaded", () => {
+    const tabs = document.querySelectorAll(".tabheader__item"),
+        tabsContent = document.querySelectorAll(".tabcontent"),
+        tabsParent = document.querySelector(".tabheader__items");
+
+    function hideTabContent() {
+        tabsContent.forEach(it => {
+            it.style.display = "none";
+            //console.log(it, "is done");
+        });
+
+        tabs.forEach(tab => {
+            tab.classList.remove("tabheader__item_active");
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].style.display = "block";
+        tabs[i].classList.add("tabheader__item_active");
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener("click", (e) => {
+        const target = e.target;
+
+        if (target && target.classList.contains("tabheader__item")) {
+            tabs.forEach((tab, i) => {
+                if (target == tab) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+
+    });
+
+});
