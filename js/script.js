8fb97fa468b338a21790cbdab1d6154b00050e5d
Ilia Dev <bridatal@gmail.com>
2023-11-27 19:43:05 +0400

    Third edition

--- js/script.js
+++ js/script.js
@@ -85,4 +85,34 @@ window.addEventListener("DOMContentLoaded", () => {
 
     setClock(".timer", deadline);
 
+    //Modal window
+    const modalTrigger = document.querySelectorAll("[data-open]"),
+        modal = document.querySelector(".modal"),
+        modalCloseBtn = document.querySelector("[data-close]");
+
+    modalTrigger.forEach(it => {
+        it.addEventListener("click", () => {
+            modal.style.display = "block";
+            document.body.style.overflow = "hidden";
+        });
+    });
+
+    function closeModal() {
+        modal.style.display = "none";
+        document.body.style.overflow = "";
+    }
+    modalCloseBtn.addEventListener("click", closeModal);
+
+    modal.addEventListener("click", (e) => {
+        if (e.target == modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener("keydown", (e) => {
+        if (e.code == "Escape" && modal.style.display == "block") {
+            closeModal();
+            console.log(e.code);
+        }
+    });
 });
